fec27ddbca4d97531e692ba1f25aa103ce187e9b
liellnima <44509330+liellnima@users.noreply.github.com>
2019-02-19 21:38:16 +0100

    Add files via upload
    WareHouseModel is split up into State and WareHouseModel now. State does the representation. 
    Everything should work, TestState is a test class, which can be ignored (just there such that I could test that I have not done mistakes).
    Contact me, if there are any problems. I will upload a concrete description about how to use the State class.

--- Intelligent Warehouse System/src/model/WareHouseModel.java
+++ Intelligent Warehouse System/src/model/WareHouseModel.java
@@ -6,23 +6,38 @@ public class WareHouseModel {
 
 	private String order;
 	private SearchAlgorithm alg;
-	private WareHouse wareHouse;
+	private HashMap<int, String[]> wareHouse = new HashMap<int, String>;
 	private String result;
-	
-	
-	private void generateState() {
-		//create first state
+
+	public WareHouseModel(){
+		//leerer Konstruktor, damit Algorithmen diese Klasse
+		//erstellen koennen
 	}
-	
-	public void setWarehouse(String wareHousePath) {
-		wareHouse = new WareHouse(wareHousePath);
-	}
-	
-	public void startSearch(SearchAlgorithm alg, String order) {
+	/**
+	 *@return boolean true if setWarehouse was successful
+	 * false if setWarehouse was not successful
+	 */
+	public boolean setWarehouse(File warehouseTxt){
+	//dafuer sorgen, dass aus dem txt Tile eine
+	//HashMap wird mit zeilen als key uebersetzt und
+	//Stringarrays als value
+	//put that in a try-catch thing!
+ }
+
+	public void startSearch(SearchAlgorithm alg, String[] order) {
 		result = alg.search(order, wareHouse);
+
+		//ruft generateState
 	}
-	
+
+
+	//get PSU number
+	//returns String with elements
+
 	public String getResult() {
+		//anzahl der PSUs used
+		//all used Psus identifiers
+		//
 		return result;
 	}
 
