889699dc384ddaae97bcfe408cc64777454ae491
malexandrovs <michael.alexandrovsky@gmail.com>
2019-02-26 20:08:54 +0100

    Implemented the startSearch method in IntelligentWareHouseSystem.java renamed Variable in WarehouseController.java

--- intelligentwarehousesystem/src/model/WareHouseModel.java
+++ intelligentwarehousesystem/src/model/WareHouseModel.java
@@ -9,64 +9,80 @@ import algorithms.Algorithms;
 
 public class WareHouseModel {
 
-	private String order;
-	private SearchAlgorithm alg;
-	private HashMap<String, ArrayList<Integer>> wareHouse = new HashMap<String, ArrayList<Integer>>();
+	// private String order;
+	private Algorithms algs;
+	private HashMap<String, ArrayList<Integer>> wareHouse;
 	private String result;
+	private StateHandler stateHandler;
 
 	public WareHouseModel(){
-
+		wareHouse = new HashMap<String, ArrayList<Integer>>();
 	}
 
 	/**
-	 *@return boolean true if setWarehouse was successful
-	 * false if setWarehouse was not successful
+	 * Method to build the Warehouse representation. It receives a File-Object with the Warehouse.txt and builds
+	 * a HashMap with the Items as keys and the ArrayLists of PSUs that contain the items as values.
+	 *@return boolean true if setWarehouse was successful, false otherwise.
 	 */
 	public boolean setWarehouse(File warehouseTxt){
-	//dafuer sorgen, dass aus dem txt Tile eine
-	//HashMap wird mit zeilen als key uebersetzt und
-	//Stringarrays als value
-	//put that in a try-catch thing!
 
+		boolean successful = true;
 	
 
-	try (
-		FileReader fr = new FileReader(warehousetxt);
-		BufferedReader br = new BufferedReader(fr);
-	){
-		String[] itmes = br.readLine().split(" ");
-		for (String item : items) {
-			wareHouse.put(item, new ArrayList<Integer>());
-		}
-
-		//skip empty line
-		br.readLine();
-
-		String line = br.readLine();
-		int psu = 1;
-
-		while (line !=  null) {
+		try (
+			FileReader fr = new FileReader(warehouseTxt);
+			BufferedReader br = new BufferedReader(fr);
+		){
+			String[] itmes = br.readLine().split(" ");
 			for (String item : items) {
-				if (line.contains(item)) {
-					wareHouse.get(item).add(psu);
-				}
+				wareHouse.put(item, new ArrayList<Integer>());
 			}
 
-			line = br.readLine();
-			i++;
-			
+			// skip empty line
+			br.readLine();
+
+			String line = br.readLine();
+			int psu = 0;
+
+			while (line !=  null) {
+				for (String item : items) {
+					if (line.contains(item)) {
+						wareHouse.get(item).add(psu);
+					}
+				}
+				line = br.readLine();
+				i++;	
+			}
+		} catch (IOException e) {
+			successful = false;
 		}
+		stateHandler = StateHandler.getInstance(wareHouse);
+		alg = new Algorithms(stateHandler);
 
-
-	} catch (IOException e) {
-		//TODO: handle exception
-	}
+		return successful;
  }
 
-	public void startSearch(SearchAlgorithm alg, String[] order) {
-		result = alg.search(order, wareHouse);
-
-		//ruft generateState
+ 	/**
+	  * Method to coordinate the actual search process.
+	  * @param alg is the code for the search algorithm.
+	  * @param param is the optional parameter that depends on the search algorithm
+	  * @param order
+	  */
+	public void startSearch(int alg, int param, String[] order) {
+		
+		stateHandler.setOrder(order);
+		int[] initialState = stateHandler.getInitialState();
+		switch (alg){
+			case 0: result = algs.hillClimbing(initialState);
+				break;
+			case 1: result = algs.simmulatedAnnealing(initialState);
+				break;
+			case 2: result = algs.localBeam(initialState, param);
+				break;
+			case 3: result = algs.randomRestartHillClimbin(initialState, param);
+				break;
+			case 4: result = firstChoiceHillClimbing(initialState);
+		}
 	}
 
 
