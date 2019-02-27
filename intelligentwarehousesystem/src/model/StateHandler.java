6988a9039e602bf76923fe4fe0754a903b38877c
liellnima <44509330+liellnima@users.noreply.github.com>
2019-02-27 21:15:27 +0100

    Update StateHandler.java
    eval

--- intelligentwarehousesystem/src/model/StateHandler.java
+++ intelligentwarehousesystem/src/model/StateHandler.java
@@ -13,8 +13,9 @@ import java.util.stream.Stream;
 public class StateHandler {
 
   private String[] order = null;
-  private HashMap<String, ArrayList<Integer>> warehouse = new HashMap<String, ArrayList<Integer>>();
-  private static StateHandler stateInstance = null;
+  private Map<String, ArrayList<Integer>> warehouse = new HashMap<String, ArrayList<Integer>>();
+  //key: PSU number; value: how many elements of the order are contained in the PSU
+  private Map<Integer, Integer> evaluationMap = new TreeMap<Integer, Integer>();
 
   /** Constructor for creating a StateHandler.
    * @param warehouse of type HashMap<String, ArrayList<Integer>>.
@@ -40,6 +41,25 @@ public class StateHandler {
       this.warehouse = newWarehouse;
     }
   }
+  /**
+   *
+   */
+  public int evaluate(int[] state){
+    //go through the evaluationMap and give each state the number
+    //assigned as value
+    //add everything together
+    int evaluationResult = -1;
+    int[] consideredValues = new int[state.length];
+    for(int psu = 0; psu < state.length; psu++){
+      int valueOfPSU = this.evaluationMap.get(state[psu]);
+      evaluationResult = evaluationResult + valueOfPSU;
+      consideredValues[psu] = valueOfPSU;
+    }
+    int bestPSU = Arrays.stream(consideredValues).max().getAsInt();
+    //e.g. for an order of 5 items: if the first psu contains five items and the rest only 1,
+    //that should be stil better than a 5 psus containing all 4 elements of the order!
+    return evaluationResult + (bestPSU * 100) - bestPSU;
+  }
 
   /**
     *EVALUATION FUNCTION
@@ -52,7 +72,7 @@ public class StateHandler {
     *handeled.
     *@param state, current state which shall be evaluated.
     */
-  public int evaluate(int[] state){
+  public int evaluateNotPrecise(int[] state){
     int result = -2;
     if(state.length < 1){
       System.out.println("@StateHandler: An empty State cannot be evaluated");
@@ -74,19 +94,18 @@ public class StateHandler {
     return result;
   }
 
-  /** Creates the neighbours of a given State.
+	/** Creates the neighbours of a given State.
    * Neighbours which are searched: For each element of the
    * order there is a new PSU found, containing that item.
    * A neighbours differs only in one PSU from the current State.
-   *@return ArrayList<int[]> with all neighbours, does not skip indices and starts with 0.
+	 *@return ArrayList<int[]> with all neighbours, does not skip indices and starts with 0.
    *@param currentState of type int[]: from where on the next neighbours
    * should be searched.
    *@param stopAtEndOfWarehouse of type boolean. If true, it will return -1 for items,
    * if it has reached the End of the warehouse. Ff false, the next neighbor will be choosen from
    * the beginning of the beginning of the warehouse. In this case there should be an iteration limit.
-   */
-    public ArrayList<int[]> createNeighbours(int[] currentState, boolean stopAtEndOfWarehouse){
-    System.out.println("Creating Neighbours");
+	 */
+	public ArrayList<int[]> createNeighbours(int[] currentState, boolean stopAtEndOfWarehouse){
     ArrayList<int[]> neighbours = new ArrayList<int[]>();
 
     for(int i = 0; i < order.length; i++){
@@ -115,7 +134,10 @@ public class StateHandler {
         System.out.println("@StateHandler: Item does not exist");
       }
     }
-    System.out.println(neighbours.size());
+//    for(int i = 0; i < neighbours.size(); i++){
+//      int result = this.evaluate(neighbours.get(i));
+//      System.out.println("Value of Neighbour:" + result);
+//    }
     return neighbours;
   }
 
@@ -167,7 +189,8 @@ public class StateHandler {
 
   /**Method for changing or setting the order.
    * Order hast to be set before generateInitialState
-   * generateRandomState or createNeighbours.
+   * generateRandomState or createNeighbours. We also initialize
+   * values for our evaluation function here.
    *@param order of type String[] which should be set.
    */
   public void setOrder(String[] order){
@@ -175,6 +198,27 @@ public class StateHandler {
       System.err.println("@StateHandler: Order was not set, since it was empty or null");
     } else {
       this.order = order;
+      //wir gehen durch die Order
+      //und packen jeden neuen PSU in die HashMap
+
+      for(int item = 0; item < order.length; item++){
+        //we get the psus of the order
+        ArrayList<Integer> allPSUsContainingItem = warehouse.get(order[item]);
+        if(allPSUsContainingItem == null){
+          System.out.println("Nuuuuuull");
+        }
+        //we go through all PSUS
+        for(int psu = 0; psu < allPSUsContainingItem.size(); psu++){
+          Integer onePSUContaingItem = allPSUsContainingItem.get(psu);
+          //key is psu Number, value number of ordered items it contains
+          //if psu is contained, increment value, else put the new key with value 1
+          if(evaluationMap.containsKey(onePSUContaingItem)){
+            this.evaluationMap.put(onePSUContaingItem, this.evaluationMap.get(onePSUContaingItem) + 1);
+          } else if(!(evaluationMap.containsKey(onePSUContaingItem))){
+            this.evaluationMap.put(onePSUContaingItem, 1);
+          }
+        }
+      }
     }
   }
 
@@ -191,7 +235,7 @@ public class StateHandler {
    * a state can reach via {@evaluate}
    */
   public int optimum(){
-    return order.length - 1;
+    return 500;
   }
 
   /**Checks if given state is a valid state. Valid states do not contain -1.
@@ -209,12 +253,48 @@ public class StateHandler {
     List<Integer> list = Arrays.stream(state).boxed().collect(Collectors.toList());
     return (!list.contains(-1));
   }
+  /**
+   *@return int[] containing i ints. The ith value corresponds to
+   * the ith ordered item.
+   */
+  public int[] showUsedPSUs(int[] state){
+    System.out.println("State: " + Arrays.toString(state));
+    int[] duplicates = new int[order.length];
+
+    //go through order and find for each item the psu with the highest value
+    for(int item = 0; item < order.length; item++){
+      boolean bestPSUFound = false;
+      //evaluationMap is a treeMap -> ascending order -> highest value at the end
+      for(int i = evaluationMap.size()-1; i >= 0 && !bestPSUFound; i--){
+        //getting the key of current PSU (beginning at the end of the treemap)
+        Integer currentPSU = (Integer) evaluationMap.keySet().toArray()[i];
+        if((warehouse.get(order[item])).contains(currentPSU)){
+          bestPSUFound = true;
+          duplicates[item] = currentPSU;
+        }
+      }
+    }
+    System.out.println("Used PSUS: " + Arrays.toString(duplicates));
+    //removing duplicates
+    Set<Integer> withoutDuplicates = new HashSet<Integer>();
+    for(int i = 0; i < duplicates.length; i++){
+      withoutDuplicates.add(duplicates[i]);
+    }
+    //convert to int[]
+    Integer[] result = withoutDuplicates.toArray(new Integer[withoutDuplicates.size()]);
+    return Arrays.stream(result).mapToInt(Integer::intValue).toArray();
+  }
+
+  public int numOfUsedPSUs(int[] state){
+    int[] stateResult = this.showUsedPSUs(state);
+    return stateResult.length;
+  }
 
   /** Returns the number of used PSUs.
    *@return number of used PSUs in a state.
    *@param state of type[] of which we want to know the number of used PSUS.
    */
-  public int numOfUsedPSUs(int[] state){
+  public int numOfUsedPSUsNotPrecise(int[] state){
     int result;
     Set<Integer> stateAsSet = new HashSet<Integer>();
     for(int i = 0; i < state.length; i++){
@@ -233,7 +313,7 @@ public class StateHandler {
    *If it contains a -1 at least one item was not found.
    *@return an int[] with the identifiers of the used PSUs.
    */
-  public int[] showUsedPSUs(int[] state){
+  public int[] showUsedPSUsNotPrecise(int[] state){
     //short way for getting a set, runtime is still the same.
     Set<Integer> hashSet = new HashSet<Integer>();
     for(int i = 0; i < state.length; i++){
