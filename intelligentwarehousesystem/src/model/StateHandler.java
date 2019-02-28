ad43ab0c20adf37d9ea9787bf72f91fd8278c9f9
liellnima <44509330+liellnima@users.noreply.github.com>
2019-02-28 02:17:40 +0100

    Update StateHandler.java
    new Evaluation. ugly, but working.

--- intelligentwarehousesystem/src/model/StateHandler.java
+++ intelligentwarehousesystem/src/model/StateHandler.java
@@ -2,6 +2,7 @@ package model;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import static java.util.Map.Entry.*;
 
 /** Class State provides methods for operatig one states.
  * A warehouse and an order needs to be initalized for the most methods.
@@ -13,8 +14,10 @@ import java.util.stream.Stream;
 public class StateHandler {
 
   private String[] order = null;
-  private HashMap<String, ArrayList<Integer>> warehouse = new HashMap<String, ArrayList<Integer>>();
-  private static StateHandler stateInstance = null;
+  private Map<String, ArrayList<Integer>> itemWarehouse = new HashMap<String, ArrayList<Integer>>();
+  private Map<Integer, String[]> psuWarehouse = new HashMap<Integer, String[]>();
+  //key: PSU number; value: how many elements of the order are contained in the PSU
+  private Map<Integer, Integer> evaluationMap = new HashMap<Integer, Integer>();
 
   /** Constructor for creating a StateHandler.
    * @param warehouse of type HashMap<String, ArrayList<Integer>>.
@@ -23,76 +26,73 @@ public class StateHandler {
    * the items. Do not skip indices of the ArrayList. Start with index 0.
    * Do not use empty warehouse or a null object.
    */
-  public StateHandler(HashMap<String, ArrayList<Integer>> warehouse){
-    if(warehouse == null || warehouse.size() == 0){
-      System.err.println("@StateHandler: Warehouse is null or empty.");
+  public StateHandler(HashMap<String, ArrayList<Integer>> itemWarehouse, HashMap<Integer, String> psuWarehouseString){
+    if(itemWarehouse == null || itemWarehouse.size() == 0){
+      System.err.println("@StateHandler: Item Warehouse is null or empty.");
     }
-    this.warehouse = warehouse;
+    this.itemWarehouse = itemWarehouse;
+    if(psuWarehouseString == null || psuWarehouseString.size() == 0){
+      System.err.println("@StateHandler: PSU Warehouse is null or empty.");
+    }
+    for(int psu = 0; psu < psuWarehouseString.size(); psu++){
+      psuWarehouse.put(psu, psuWarehouseString.get(psu).split(" "));
+    }
+
   }
 
   /** Updates the warehouse, replaces the current warehouse with a new one.
    *@param newWarehouse of type HashMap<String, ArrayList<Integer>>. The new warehouse.
    */
-  public void updateWarehouse(HashMap<String, ArrayList<Integer>> newWarehouse){
-    if(newWarehouse == null || newWarehouse.size() < 1){
-      System.err.println("@StateHandler: New Warehouse was not set since it is empty or null.");
-    } else{
-      this.warehouse = newWarehouse;
+  public void updateWarehouse(HashMap<String, ArrayList<Integer>> itemWarehouse, HashMap<Integer, String> psuWarehouseString){
+    if(itemWarehouse == null || itemWarehouse.size() == 0){
+      System.err.println("@StateHandler: itemWarehouse is null or empty.");
+    }
+    this.itemWarehouse = itemWarehouse;
+    if(psuWarehouseString == null || psuWarehouseString.size() == 0){
+      System.err.println("@StateHandler: psuWarehouse are null or empty.");
+    }
+    for(int psu = 0; psu < psuWarehouseString.size(); psu++){
+      psuWarehouse.put(psu, psuWarehouseString.get(psu).split(" "));
     }
   }
-
   /**
-    *EVALUATION FUNCTION
-    *@return int, a value which expresses how good
-    *or bad the state is. The higher the number, the better.
-    *The maximum is the number of elements minus 1 -> only one set is used.
-    *The minimum is 0 -> each element uses one set.
-    *Returns -1 if the element could not be found in the psus.
-    *Return -2, if the for-loop was not entered -> severe problem which should be
-    *handeled.
-    *@param state, current state which shall be evaluated.
-    */
+   *
+   */
   public int evaluate(int[] state){
-    int result = -2;
-    if(state.length < 1){
-      System.out.println("@StateHandler: An empty State cannot be evaluated");
-      return result;
+    //go through the evaluationMap and give each state the number
+    //assigned as value
+    //add everything together
+    int evaluationResult = -1;
+    int[] consideredValues = new int[state.length];
+    for(int psu = 0; psu < state.length; psu++){
+      int valueOfPSU = this.evaluationMap.get(state[psu]);
+      evaluationResult = evaluationResult + valueOfPSU;
+      consideredValues[psu] = valueOfPSU;
     }
-    Set<Integer> stateAsSet = new HashSet<Integer>();
-    for(int i = 0; i < state.length; i++){
-      if(state[i] == -1){
-        return -1;
-      }
-      stateAsSet.add(Integer.valueOf(state[i]));
-    }
-    //result is number of elements minus number of used sets
-    result = state.length - stateAsSet.size();
-    //for unexcpected other exceptions
-    if(result == -2){
-      System.out.println("@StateHandler: For-Loop was not entered in the evaluate function.");
-    }
-    return result;
+    int bestPSU = Arrays.stream(consideredValues).max().getAsInt();
+    //e.g. for an order of 5 items: if the first psu contains five items and the rest only 1,
+    //that should be stil better than a 5 psus containing all 4 elements of the order!
+    return evaluationResult + (bestPSU * 100) - bestPSU;
   }
 
-  /** Creates the neighbours of a given State.
+	/** Creates the neighbours of a given State.
    * Neighbours which are searched: For each element of the
    * order there is a new PSU found, containing that item.
    * A neighbours differs only in one PSU from the current State.
-   *@return ArrayList<int[]> with all neighbours, does not skip indices and starts with 0.
+	 *@return ArrayList<int[]> with all neighbours, does not skip indices and starts with 0.
    *@param currentState of type int[]: from where on the next neighbours
    * should be searched.
    *@param stopAtEndOfWarehouse of type boolean. If true, it will return -1 for items,
    * if it has reached the End of the warehouse. Ff false, the next neighbor will be choosen from
    * the beginning of the beginning of the warehouse. In this case there should be an iteration limit.
-   */
-    public ArrayList<int[]> createNeighbours(int[] currentState, boolean stopAtEndOfWarehouse){
-    System.out.println("Creating Neighbours");
+	 */
+	public ArrayList<int[]> createNeighbours(int[] currentState, boolean stopAtEndOfWarehouse){
     ArrayList<int[]> neighbours = new ArrayList<int[]>();
 
     for(int i = 0; i < order.length; i++){
       int[] currentNeighbour = currentState.clone();
       String currentItem = order[i];
-      ArrayList<Integer> allPSUsContainingItem = warehouse.get(currentItem);
+      ArrayList<Integer> allPSUsContainingItem = itemWarehouse.get(currentItem);
       int currentPSU = currentState[i];
       //otherwise we do not need to search for any neighbours
       if((currentPSU != -1) & (allPSUsContainingItem != null)){
@@ -115,7 +115,10 @@ public class StateHandler {
         System.out.println("@StateHandler: Item does not exist");
       }
     }
-    System.out.println(neighbours.size());
+//    for(int i = 0; i < neighbours.size(); i++){
+//      int result = this.evaluate(neighbours.get(i));
+//      System.out.println("Value of Neighbour:" + result);
+//    }
     return neighbours;
   }
 
@@ -132,7 +135,7 @@ public class StateHandler {
     //catch for each element of the order a random number of the corresponding ArrayList<Integer>
     for(int i = 0; i < order.length; i++){
       String currentItem = order[i];
-      ArrayList<Integer> allPSUsContainingItem = warehouse.get(currentItem);
+      ArrayList<Integer> allPSUsContainingItem = itemWarehouse.get(currentItem);
       if(allPSUsContainingItem == null || allPSUsContainingItem.size() == 0){
         randomState[i] = -1;
       } else {
@@ -155,7 +158,7 @@ public class StateHandler {
     //the first PSU contained in the ArrayList "allPSUsContainingItem"
     for(int i = 0; i < order.length; i++){
       String currentItem = order[i];
-      ArrayList<Integer> allPSUsContainingItem = warehouse.get(currentItem);
+      ArrayList<Integer> allPSUsContainingItem = itemWarehouse.get(currentItem);
       if(allPSUsContainingItem == null || allPSUsContainingItem.size() == 0){
         initialState[i] = -1;
       } else {
@@ -167,7 +170,8 @@ public class StateHandler {
 
   /**Method for changing or setting the order.
    * Order hast to be set before generateInitialState
-   * generateRandomState or createNeighbours.
+   * generateRandomState or createNeighbours. We also initialize
+   * values for our evaluation function here.
    *@param order of type String[] which should be set.
    */
   public void setOrder(String[] order){
@@ -175,6 +179,19 @@ public class StateHandler {
       System.err.println("@StateHandler: Order was not set, since it was empty or null");
     } else {
       this.order = order;
+      List<String> orderList = Arrays.asList(this.order);
+      //initialize EvaluationMap
+      //each psu gets a value describing how many items of the order it holds
+      for(int i = 0; i < psuWarehouse.size(); i++){
+        String[] allItems = psuWarehouse.get(i);
+        int valueOfPSU = 0;
+        for(int j = 0; j < allItems.length; j++){
+          if(orderList.contains(allItems[j])){
+            valueOfPSU++;
+          }
+        }
+        evaluationMap.put(Integer.valueOf(i), Integer.valueOf(valueOfPSU));
+      }
     }
   }
 
@@ -183,15 +200,6 @@ public class StateHandler {
    */
   public String[] getOrder(){
     return this.order;
-  }
-
-  /** Return the best value a state can reach
-   * via evaluate with the current order.
-   * @return int which is the max value
-   * a state can reach via {@evaluate}
-   */
-  public int optimum(){
-    return order.length - 1;
   }
 
   /**Checks if given state is a valid state. Valid states do not contain -1.
@@ -209,37 +217,60 @@ public class StateHandler {
     List<Integer> list = Arrays.stream(state).boxed().collect(Collectors.toList());
     return (!list.contains(-1));
   }
-
-  /** Returns the number of used PSUs.
-   *@return number of used PSUs in a state.
-   *@param state of type[] of which we want to know the number of used PSUS.
+  /**
+   *@return int[] containing i ints. The ith value corresponds to
+   * the ith ordered item.
    */
-  public int numOfUsedPSUs(int[] state){
-    int result;
-    Set<Integer> stateAsSet = new HashSet<Integer>();
+  public int[] showUsedPSUs(int[] state){
+    List<Integer> usedPSUs = new ArrayList<Integer>();
+    List<String> orderList = new LinkedList<String>(Arrays.asList(this.order));
+
+    //we create a map with all psus of the state and their values
+    Map<Integer, Integer> psusWithValue = new HashMap<Integer, Integer>();
+    for(int j = 0; j < state.length; j++){
+      Integer value = evaluationMap.get(Integer.valueOf(state[j]));
+      psusWithValue.put(Integer.valueOf(state[j]), value);
+    }
+    System.out.println(psusWithValue);
+
+    //we sort state[] -> psu with highest value at the beginning (descending order)
+    for(int k = 0; k < psusWithValue.size(); k++){
+      int maxValue= Collections.max(psusWithValue.values());
+      boolean maxFound = false;
+      for(int i = 0; i < psusWithValue.size() & !maxFound; i++){
+        Integer currentPSU = (Integer) psusWithValue.keySet().toArray()[i];
+        if(psusWithValue.get(currentPSU) == maxValue){
+          state[k] = currentPSU;
+          psusWithValue.put(currentPSU, -1);
+          maxFound = true;
+        }
+      }
+    }
+    //we go through the ordered states and add the psu to the usedPSU List while removing the
+    //considered items from the order List
     for(int i = 0; i < state.length; i++){
-      stateAsSet.add(Integer.valueOf(state[i]));
+      int currentPSU = state[i];
+      String[] itemsContainedByPSU = psuWarehouse.get(currentPSU);
+      for(int j = 0; j < itemsContainedByPSU.length; j++){
+        String item = itemsContainedByPSU[j];
+        if(orderList.contains(item)){
+          if(!(usedPSUs.contains(currentPSU))){
+              usedPSUs.add(Integer.valueOf(currentPSU));
+          }
+          orderList.remove(item);
+        }
+      }
     }
-    result = stateAsSet.size();
-    if(stateAsSet.contains(Integer.valueOf(-1))){
-      result = result -1;
-      System.out.println("@StateHandler: Attention, State for which number of used PSUs was asked is not valid.");
+    int[] result = new int[usedPSUs.size()];
+    for(int k = 0; k < result.length; k++){
+      result[k] = usedPSUs.get(k);
     }
+    System.out.println("@StateHandler: used PSUs" + Arrays.toString(result));
     return result;
   }
 
-  /** Shows the PSU a State uses without duplicates.
-   *@param state of type int[], of which we want to know the used PSUs.
-   *If it contains a -1 at least one item was not found.
-   *@return an int[] with the identifiers of the used PSUs.
-   */
-  public int[] showUsedPSUs(int[] state){
-    //short way for getting a set, runtime is still the same.
-    Set<Integer> hashSet = new HashSet<Integer>();
-    for(int i = 0; i < state.length; i++){
-      hashSet.add(Integer.valueOf(state[i]));
-    }
-    Integer[] result = hashSet.toArray(new Integer[hashSet.size()]);
-    return Arrays.stream(result).mapToInt(Integer::intValue).toArray();
+  public int numOfUsedPSUs(int[] state){
+    int[] stateResult = this.showUsedPSUs(state);
+    return stateResult.length;
   }
 }
